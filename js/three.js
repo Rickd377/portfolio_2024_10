c9b7f78dd80b5a16fea28269d67ab3a2693c3a35
Rick <rick-2007@outlook.com>
2024-10-24 12:27:19 +0200

    formatted all scss code

--- js/three.js
+++ js/three.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
-        initThreeJS();
-    }
+  if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+    initThreeJS();
+  }
 });
 
 function initThreeJS() {
-    import("three").then(THREE => {
-        const renderer = new THREE.WebGLRenderer({ antialias: false });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-        renderer.setClearColor(0x0d0d0d);
+  import("three").then(THREE => {
+    const renderer = new THREE.WebGLRenderer({ antialias: false });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    renderer.setClearColor(0x0d0d0d);
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
-        camera.position.z = 0;
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
+    camera.position.z = 0;
 
-        const scene = new THREE.Scene();
-        const geometry = new THREE.IcosahedronGeometry(1, 2);
-        const material = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
-        const mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+    const scene = new THREE.Scene();
+    const geometry = new THREE.IcosahedronGeometry(1, 2);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-        const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x7a7a7a, wireframe: true });
-        const wireframe = new THREE.Mesh(geometry, wireframeMaterial);
-        wireframe.scale.multiplyScalar(1.001);
-        mesh.add(wireframe);
+    const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x7a7a7a, wireframe: true });
+    const wireframe = new THREE.Mesh(geometry, wireframeMaterial);
+    wireframe.scale.multiplyScalar(1.001);
+    mesh.add(wireframe);
 
-        const light = new THREE.HemisphereLight(0xffffff, 0);
-        scene.add(light);
+    const light = new THREE.HemisphereLight(0xffffff, 0);
+    scene.add(light);
 
-        function animate() {
-            requestAnimationFrame(animate);
-            mesh.rotation.y += 0.0005;
-            mesh.rotation.x -= 0.0005;
-            renderer.render(scene, camera);
-        }
+    function animate() {
+      requestAnimationFrame(animate);
+      mesh.rotation.y += 0.0005;
+      mesh.rotation.x -= 0.0005;
+      renderer.render(scene, camera);
+    }
 
-        animate();
+    animate();
 
-        window.addEventListener("resize", () => {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            renderer.setSize(width, height);
-            camera.aspect = width / height;
-            camera.updateProjectionMatrix();
-        });
+    window.addEventListener("resize", () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
     });
+});
 }
